8089584937b5e3010deea849435348ab5972fb22
Vladyslav Kliucharov <kliucharovvlad@gmail.com>
2024-02-05 16:16:52 +0100

    Updated

--- demo1.js
+++ demo1.js
@@ -1,11 +1,14 @@
 module.exports = async function(context, commands) {
-    // We start by navigating to the login page.
-    await commands.navigate('https://www.google.com/')
+    const seleniumWebdriver = context.selenium.webdriver
+    const seleniumDriver = context.selenium.driver
+    seleniumDriver.manage().window().maximize()
+        // We start by navigating to the login page.
+    await commands.measure.start("01_Open_Main_Page");
+    await commands.navigate('https://jmeter.apache.org/')
 
     // Add text into an input field, finding the field by id
 
-    await commands.measure.start('https://www.google.com/doodles')
-
-    return commands.measure.start('https://www.google.com/doodles/croatia-statehood-day-2024')
-
+    await commands.wait.byXpath('//*[contains(text(), "Distributed Testing")]', 60000);
+    await commands.click.byXpath('//*[contains(text(), "Distributed Testing")]');
+    await commands.measure.stop();
 }
